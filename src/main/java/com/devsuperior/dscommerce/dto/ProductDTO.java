0d2a5d574c7a39ed022fc177ba3a449e4a1e2af7
Handerson Barreto <handersonaires@gmail.com>
2025-02-18 13:58:46 -0300

    GET Product Paged

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO(){
+
+    }
+
+    public ProductDTO(Product entity) {
+        imgUrl = entity.getImgUrl();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        name = entity.getName();
+        id = entity.getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
